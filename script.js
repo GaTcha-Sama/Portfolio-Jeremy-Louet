585620f10c52567e915529e8d80f5e131f312644
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-03 20:28:26 +0200

    Ajout version en anglais + debug script

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-// Toggle icon navbar //
+// Affichage projets bouton "En savoir plus"-"Réduire" //
 
-let menuIconBar = document.querySelector('#menu-icon');
-let menuIconX = document.querySelector('fa-xmark');
-let navbar = document.querySelector('.navbar');
+document.addEventListener("DOMContentLoaded", function() {
+    let buttonMore = document.querySelectorAll("#more");
+    let description = document.querySelectorAll(".services-description");
+  
+    buttonMore.forEach((button, index) => {
+      button.addEventListener("click", function(e) {
+        console.log(e, "bouton cliqué");
+        e.stopPropagation();
 
-menuIconBar.onclick = () => {
-    menuIconBar.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
-    navbar.style.display = "block";
-};
-
-menuIconX.onclick = () => {
-    menuIconX.classList.toggle('fa-bars');
-    navbar.classList.toggle('inactive');
-    navbar.style.display = "none";    
-};
+        // Obtenir la description correspondante au bouton cliqué
+        let currentDescription = description[index];
+  
+        if (currentDescription.style.display === "none") {
+          currentDescription.style.display = "block";
+          button.textContent = "Réduire";
+        } else {
+          currentDescription.style.display = "none";
+          button.textContent = "En savoir plus";
+        }
+        
+      });
+    });
+});
 
 // Pour le scroll des sections //
 
+document.addEventListener("DOMContentLoaded", function() {
+
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a'); 
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -50,7 +60,7 @@ window.onscroll = () => {
     menuIcon.classList.remove('.fa-xmark');
     navbar.classList.remove('active');
 
-};
+}; 
 
 // Scroll reveal des différentes sections //
 
@@ -59,45 +69,38 @@ ScrollReveal({
     distance: '80px',
     duration: 1000,
     delay: 200
-});
+})
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
-ScrollReveal().reveal('.home-content p, .home-content li, .about-content', {origin: 'right'});
+ScrollReveal().reveal('.home-content p, .home-content li, .about-content', {origin: 'right'}); 
 
 // Défilement texte de présentation //
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Développeur Web et Mobile', 'Développeur Back End', 'un futur Dev Full Stack'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+    const typed = new Typed('.multiple-text', {
+        strings: ['Développeur Web et Mobile', 'Développeur Back End', 'un futur Dev Full Stack'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
 });
 
-// Affichage projets bouton "En savoir plus"-"Réduire" //
+/* / Toggle icon navbar //
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttonMore = document.querySelectorAll("#more");
-    let description = document.querySelectorAll(".services-description");
-  
-    buttonMore.forEach((button, index) => {
-      button.addEventListener("click", function(e) {
-        console.log(e, "bouton cliqué");
-        e.stopPropagation();
+let menuIconBar = document.querySelector('#menu-icon');
+let menuIconX = document.querySelector('fa-xmark');
+let navbar = document.querySelector('.navbar');
 
-        // Obtenir la description correspondante au bouton cliqué
-        let currentDescription = description[index];
-  
-        if (currentDescription.style.display === "none") {
-          currentDescription.style.display = "block";
-          button.textContent = "Réduire";
-        } else {
-          currentDescription.style.display = "none";
-          button.textContent = "En savoir plus";
-        }
-        
-      });
-    });
-  });
+menuIconBar.onclick = () => {
+    menuIconBar.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+    navbar.style.display = "block";
+};
+
+ menuIconX.onclick = () => {
+    menuIconX.classList.toggle('fa-bars');
+    navbar.classList.toggle('inactive');
+    navbar.style.display = "none";    
+}; */
